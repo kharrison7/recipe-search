7d5877f05f0e16f1df90993916ebd65353b9b68f
Kevin Harrison <kharrison7@elon.edu>
2017-08-08 13:58:09 -0400

    This makes images and text appear for search items.

--- main.js
+++ main.js
@@ -1,39 +1,17 @@
 console.log("JS On");
 
-
-// let url = "http://www.recipepuppy.com/api/";
-//
-// fetch(url, {
-//   method: "post",
-//   headers: {
-//     "Content-type": "http://www.recipepuppy.com/api/?q=search-term"
-//   },
-//   body: "body content goes here"
-// })
-//   // .then(json)
-//   .then(function(data) {
-//     console.log("Request succeeded with JSON response", data);
-//   })
-//   .catch(function(error) {
-//     console.log("Request failed", error);
-//   });
-
-
 function getSearch(){
   console.log("get_Search Running");
   let search_input = document.getElementById("search_items").value;
   console.log(`search_input: ` + search_input);
 
 
-
-
-
-let x = 'http://recipepuppyproxy.herokuapp.com/api/?i=';
+let x = 'http://recipepuppyproxy.herokuapp.com/api/?q=';
 let y = x + search_input;
 fetchGet(y);
 
+// THis runs the fetch.
 function fetchGet(url){
-
 fetch(url)
   .then(
     function(response) {
@@ -44,21 +22,35 @@ fetch(url)
       }
       // Examine the text in the response
       response.json().then(function(data) {
-        console.log("Data Result: " + data.results[0].title);
+        console.log("Data First Result: " + data.results[0].title);
+
+        for (let i = 0; i < 10; i++){
+            let result = data.results[i];
+            let image_Thumb = document.getElementById('fill' +i);
+            let slot = document.getElementById('slot' +i);
+            slot.innerHTML = result.title + "\nIngredients: " + result.ingredients;
+            console.log("href: " + result.href);
+            console.log("Image: " + result.thumbnail);
+
+            // This puts in 'No Image Found' if the image is absent.
+            let a = result.thumbnail;
+            if( a === '' ){
+              a = "https://www.shearwater.com/wp-content/plugins/lightbox/images/No-image-found.jpg"
+            }
+            image_Thumb.setAttribute("style", "background-image: url("+a+");");
+          }
+
 
 
 
 // This may eventually do something.
-let fill = `
-<p>${data.title}</p>
-<br>
-<p>Results: ${data.results}</p>
-
-`;
-
-
-let text_Result = document.getElementById('all_left_items');
-text_Result.innerHTML = fill;
+// let fill = `
+// <p>${data.title}</p>
+// <br>
+// <p>Results: ${data.results}</p>
+// `;
+// let text_Result = document.getElementById('all_left_items');
+// text_Result.innerHTML = fill;
 
    })
  });
