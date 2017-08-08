c30c14512dbef0f0ed48170838ab1923d249a34b
Kevin Harrison <kharrison7@elon.edu>
2017-08-08 11:39:48 -0400

    This send data from the text field once the submit button is clicked to the JS file.

--- main.js
+++ main.js
@@ -19,9 +19,19 @@ console.log("JS On");
 //   });
 
 
+function getSearch(){
+  console.log("get_Search Running");
+  let search_input = document.getElementById("search_items").value;
+  console.log(`search_input: ` + search_input);
+}
 
 
-fetch('https://api.github.com/users/kharrison7')
+let x = 'https://api.github.com/users/kharrison7';
+fetchGet(x);
+
+function fetchGet(url){
+
+fetch(url)
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -29,49 +39,9 @@ fetch('https://api.github.com/users/kharrison7')
           response.status);
         return;
       }
-
-
       // Examine the text in the response
       response.json().then(function(data) {
         console.log("data: " + data.login);
-
-
-
-// This makes the items appear on the page.
-//       let markup = `
-//
-//                   <div class="your_name">
-//                     <h1>${data.name}</h1>
-//                   </div>
-//
-// <div class="box_of_three">
-//
-//                 <div class="title1">
-//                   <h2>The Basics</h2>
-//                   <p>Name: ${data.name}</p>
-//                   <p>Github URL: <a href="${data.html_url}" class="bright">${data.login}</a></p>
-//                   <p>Email: ${email_address}</p>
-//                   <p>Company: ${data.company}</p>
-//                   <p>Website: <a href="${data.blog}" class="bright">${data.blog}</a></p>
-//                 </div>
-//
-//                 <div class="title2">
-//                   <h2>The Story</h2>
-//                   <p>Text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus arcu tellus, tincidunt quis erat quis, rhoncus cursus lacus. Fusce tincidunt metus vel convallis venenatis. Phasellus vitae tristique ligula. Integer non purus quis nibh ultrices commodo.</p>
-//                 </div>
-//
-//                  <div class="profile_Image">
-//                      <img class="img-circle" src="${data.avatar_url}" alt="profile_pic"/>
-//                  </div>
-//
-//    </div>
-//
-//
-//
-//               `
-//
-//               document.body.innerHTML = markup;
-
-              });
-
-})
+      })
+   });
+ };
