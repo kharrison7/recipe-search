bc577b7163926e4ef13d87cc76610947dba49c05
Kevin Harrison <kharrison7@elon.edu>
2017-08-08 12:59:15 -0400

    This makes the search field put in new data.

--- main.js
+++ main.js
@@ -23,11 +23,14 @@ function getSearch(){
   console.log("get_Search Running");
   let search_input = document.getElementById("search_items").value;
   console.log(`search_input: ` + search_input);
-}
 
 
-let x = 'http://recipepuppyproxy.herokuapp.com/api/?i=onions,garlic&q=omelet&p=3';
-fetchGet(x);
+
+
+
+let x = 'http://recipepuppyproxy.herokuapp.com/api/?i=';
+let y = x + search_input;
+fetchGet(y);
 
 function fetchGet(url){
 
@@ -42,6 +45,35 @@ fetch(url)
       // Examine the text in the response
       response.json().then(function(data) {
         console.log("Title of Data: " + data.title);
-      })
-   });
- };
+
+
+
+
+let fill = ``;
+
+
+
+
+
+
+
+ let clockSpot = document.querySelector( "#all_left_items" );
+ let clock       = document.createElement( "div" );
+ let liH2     = document.createElement( "h2" );
+
+ // This moves the text to the H2.
+     liH2.innerHTML = `${data.title}` ;
+ // This adds the H2 to the li tile.
+     clock.appendChild( liH2 );
+ // This adds the li to the itemList.
+     clockSpot.appendChild( clock );
+
+
+   })
+ });
+};
+
+
+}
+
+// end of getSearch function.
